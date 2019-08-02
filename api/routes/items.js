47b3fb9f8d7cbacc9e5aa37e9bbe8d6c2ea6dbef
Quinton Aiken <quintonlaiken@gmail.com>
2019-08-02 10:16:11 -0700

    refactor api into separate service

--- api/routes/items.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET items listing. */
-router.get('/', function(req, res, next) {
-  res.send([{ id: 1, title: 'PS4' }, { id: 2, title: 'Switch' }]);
-});
-
-module.exports = router;
